01620b29f32259902c846d505c2e69bf70925f8a
sujan <dev.mern.sujan@gmail.com>
2022-09-11 14:05:28 +0600

    Register Validation Done

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,29 +1,74 @@
+const { validationemail, validateLength } = require("../helpers/validation.js");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel.js");
 
 exports.register = async (req, res) => {
-  const {
-    first_name,
-    last_name,
-    username,
-    email,
-    password,
-    bYear,
-    bMonth,
-    bDay,
-    gender,
-  } = req.body;
+  try {
+    const {
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
 
-  const user = await new User({
-    first_name,
-    last_name,
-    username,
-    email,
-    password,
-    bYear,
-    bMonth,
-    bDay,
-    gender,
-  }).save();
+    // email validation
+    if (!validationemail(email)) {
+      return res.status(400).json({ message: "Invalid email address" });
+    }
 
-  res.json(user);
+    // user validation if user exist or not
+    const check = await User.findOne({ email });
+
+    if (check) {
+      res.status(400).json({
+        message:
+          "Already email is exist, Please try different email address to continue",
+      });
+    }
+
+    // text length check
+    if (!validateLength(first_name, 3, 30)) {
+      return res
+        .status(400)
+        .json({ messagae: "First name must between 3 and 30 character" });
+    }
+
+    if (!validateLength(last_name, 3, 30)) {
+      return res
+        .status(400)
+        .json({ messagae: "Last name must between 3 and 30 character" });
+    }
+
+    if (!validateLength(password, 3, 30)) {
+      return res
+        .status(400)
+        .json({ messagae: "Password must atleast 6 character" });
+    }
+
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    console.log(cryptedPassword);
+
+    return;
+
+    const user = await new User({
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
