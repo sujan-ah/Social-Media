c1b85c9a9ab6e44a97b3abf57bd25cc24fdd39fd
sujan <dev.mern.sujan@gmail.com>
2022-09-15 10:56:27 +0600

    frontend routing setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/index.js";
+import Profile from "./pages/profile/index.js";
+import Home from "./pages/home/index.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} exact></Route>
+        <Route path="/profile" element={<Profile />} exact></Route>
+        <Route path="/" element={<Home />} exact></Route>
+      </Routes>
+    </>
   );
 }
 
