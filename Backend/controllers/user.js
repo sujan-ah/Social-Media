5dda3eec62fdd7324594d2e6245b0e8ce39f5a5e
sujan <dev.mern.sujan@gmail.com>
2022-09-15 09:17:36 +0600

    activate account code done but error ache consol e

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -94,3 +94,18 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body;
+
+  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+
+  if (check.varified == true) {
+    return res
+      .status(400)
+      .json({ message: "This account is already activated" });
+  } else {
+    await User.findByIdAndUpdate(user.id, { varified: true });
+    return res.status(200).json({ message: "Account has been activated" });
+  }
+};
