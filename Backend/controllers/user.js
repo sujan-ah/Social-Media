52266246809a6a3112da92f2aeb96a0f90633688
sujan <dev.mern.sujan@gmail.com>
2022-09-15 10:14:07 +0600

    login done

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -96,16 +96,49 @@ exports.register = async (req, res) => {
 };
 
 exports.activateAccount = async (req, res) => {
-  const { token } = req.body;
+  try {
+    const { token } = req.body;
 
-  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
 
-  if (check.varified == true) {
-    return res
-      .status(400)
-      .json({ message: "This account is already activated" });
-  } else {
-    await User.findByIdAndUpdate(user.id, { varified: true });
-    return res.status(200).json({ message: "Account has been activated" });
+    if (check.varified == true) {
+      return res
+        .status(400)
+        .json({ message: "This account is already activated" });
+    } else {
+      await User.findByIdAndUpdate(user.id, { varified: true });
+      return res.status(200).json({ message: "Account has been activated" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async () => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne(email);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "This email is not connected an account" });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      res.status(400).json({ message: "Invalid password. pls try again" });
+    }
+    const token = generateToken({ id: user._id.toString() }, "7d");
+
+    res.send({
+      id: user._id,
+      username: user.username,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      varified: user.varified,
+      message: "Login Success!",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
