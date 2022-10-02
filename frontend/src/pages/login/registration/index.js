4f2778d7b691db0211d61270ff4c32f1a60e8653
sujan <dev.mern.sujan@gmail.com>
2022-10-02 19:24:04 +0600

    registration part create $ validation done

--- frontend/src/pages/login/registration/index.js
+++ frontend/src/pages/login/registration/index.js
@@ -1,14 +1,28 @@
-import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { RiEyeCloseFill, RiEyeFill } from "react-icons/ri";
 
-const Login = () => {
+const Registration = () => {
+  let [firstname, setFirstname] = useState("");
+  let [firstnameErr, setFirstnameErr] = useState("");
+  let [lastname, setLastname] = useState("");
+  let [lastnameErr, setlastnameErr] = useState("");
+
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
   let [emailErr, setEmailErr] = useState("");
   let [passwordErr, setpasswordErr] = useState("");
   let [passwordShow, setpasswordShow] = useState(false);
+
+  let handleFirstName = (e) => {
+    setFirstname(e.target.value);
+    setFirstnameErr("");
+  };
+
+  let handleLastName = (e) => {
+    setLastname(e.target.value);
+    setlastnameErr("");
+  };
 
   let handleEmail = (e) => {
     setEmail(e.target.value);
@@ -21,6 +35,12 @@ const Login = () => {
   };
 
   let handleLogin = () => {
+    if (!firstname) {
+      setFirstnameErr("First Name is required");
+    }
+    if (!lastname) {
+      setlastnameErr("Last Name is required");
+    }
     if (!email) {
       setEmailErr("Email is required");
     } else {
@@ -66,8 +86,32 @@ const Login = () => {
       <div className="md:w-[550px] md:mt-36 mt-10">
         <div className="w-full  bg-white rounded-md shadow-md px-4 py-4	">
           <input
+            onChange={handleFirstName}
+            className="w-full border border-solid border-bordercolor px-2.5 py-2.5 rounded-md "
+            type="text"
+            placeholder="First Name"
+          />
+          {firstnameErr && (
+            <p className="border border-solid bg-rose-500 px-2.5 py-2.5 rounded-md text-white font-pop font-regular mt-2.5 mb-2.5">
+              {firstnameErr}
+            </p>
+          )}
+
+          <input
+            onChange={handleLastName}
+            className="w-full border border-solid border-bordercolor px-2.5 py-2.5 rounded-md mt-4"
+            type="text"
+            placeholder="Last Name"
+          />
+          {lastnameErr && (
+            <p className="border border-solid bg-rose-500 px-2.5 py-2.5 rounded-md text-white font-pop font-regular mt-2.5 mb-2.5">
+              {lastnameErr}
+            </p>
+          )}
+
+          <input
             onChange={handleEmail}
-            className="w-full border border-solid border-bordercolor px-2.5 py-2.5 rounded-md"
+            className="w-full border border-solid border-bordercolor px-2.5 py-2.5 rounded-md mt-4"
             type="email"
             placeholder="Email address or phone number"
           />
@@ -107,7 +151,7 @@ const Login = () => {
             onClick={handleLogin}
             className="w-full bg-primary py-4 text-white font-pop font-bold text-lg	 mt-4 rounded-md"
           >
-            Log In
+            Sign Up
           </button>
           <Link
             to="/"
@@ -117,10 +161,10 @@ const Login = () => {
           </Link>
           <div className="text-center">
             <Link
-              to="/registration"
+              to="/login"
               className="inline-block bg-highlightcolor px-4 py-4 text-white font-pop font-bold text-lg mt-4 rounded-md"
             >
-              Create New Account
+              Already have
             </Link>
           </div>
         </div>
@@ -129,4 +173,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Registration;
